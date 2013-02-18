c8e6b74cbab537dfacc283c73d78d04d612b3ce7
Cameron Morrison <cameron.morrison@dingublue.com>
2013-02-18 18:20:31 +1000

    initial commit

---
+++ Sassqwatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sassqwatch
+{
+    class Program
+    {
+        private static ManualResetEvent waitHandle = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher
+                {
+                    Path = @"C:\Development\SmartClinics.HealthMax",
+                    Filter = "*.scss",
+                    IncludeSubdirectories = true
+                };
+
+            watcher.Changed += WatcherOnChanged;
+
+            watcher.EnableRaisingEvents = true;
+
+            while (true)
+            {
+                waitHandle.WaitOne();
+            }
+        }
+
+        private static void WatcherOnChanged(object sender, FileSystemEventArgs fileSystemEventArgs)
+        {
+            Console.WriteLine("{0} {1} {2}", fileSystemEventArgs.FullPath, fileSystemEventArgs.ChangeType, DateTime.Now.Ticks);
+        }
+    }
+}
